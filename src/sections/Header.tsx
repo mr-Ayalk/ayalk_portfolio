da308211e446c6b582f867e750586e88c09770e2
mr-Ayalk <ayalkteketel7@gmail.com>
2025-07-06 20:47:07 +0300

    navbar header completed

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,13 +1,15 @@
 export const Header = () => {
   return(
 
-    <div>
+    <div className="flex justify-center items-center relative top-3">
      
-   <nav>
-    <a href="#">Home</a>
-    <a href="">Projects</a>
-    <a href="">About</a>
-    <a href="">Contact</a>
+   <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+    <a href="#"
+    className="nav-item"
+    >Home</a>
+    <a href="" className="nav-item">Projects</a>
+    <a href="" className="nav-item">About</a>
+    <a href="" className="nav-item text-gray-900 bg-white hover:bg-white/70 hover:text-gray-900">Contact</a>
    </nav>
     
     </div>
