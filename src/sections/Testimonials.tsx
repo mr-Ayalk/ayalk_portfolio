cec7c076836c24e6ffc8141b10a9596820a73bba
mr-Ayalk <ayalkteketel7@gmail.com>
2025-07-07 15:29:54 +0300

    tablet size testemonial page completed

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -55,7 +55,10 @@ export const TestimonialsSection = () => {
         <div className="mt-16 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
           <div className="flex gap-8 flex-none">
             {testimonials.map((testimonial) => (
-              <Card key={testimonial.name} className="max-w-xs">
+              <Card
+                key={testimonial.name}
+                className="max-w-xs md:max-w-md md:p-8"
+              >
                 <div className="flex gap-4 items-center">
                   <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0">
                     <Image
@@ -67,12 +70,12 @@ export const TestimonialsSection = () => {
 
                   <div className="">
                     <div className="font-semibold">{testimonial.name}</div>
-                    <div className="text-sm text-white/40">
+                    <div className="text-sm md:text-base text-white/40">
                       {testimonial.position}
                     </div>
                   </div>
                 </div>
-                <p className="mt-4 text-sm">{testimonial.text}</p>
+                <p className="mt-4 md:mt-6 text-sm">{testimonial.text}</p>
               </Card>
             ))}
           </div>
