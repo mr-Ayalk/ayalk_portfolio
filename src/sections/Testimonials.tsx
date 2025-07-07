eabff3cf35607a4abbb5940bdf3583f003af4c00
mr-Ayalk <ayalkteketel7@gmail.com>
2025-07-07 15:26:34 +0300

    mobile size testemonial page completed

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -43,7 +43,7 @@ const testimonials = [
 
 export const TestimonialsSection = () => {
   return (
-    <div className="py-16">
+    <div className="py-16 flex">
       <div className="container">
         <SectionHeader
           eyebrow="Happy Clients"
@@ -52,15 +52,30 @@ export const TestimonialsSection = () => {
         about my work."
         />
 
-        <div>
-          {testimonials.map((testimonial) => (
-            <Card key={testimonial.name}>
-              <Image src={testimonial.avatar} alt={testimonial.name} />
-              <div>{testimonial.name}</div>
-              <div>{testimonial.position}</div>
-              <p>{testimonial.text}</p>
-            </Card>
-          ))}
+        <div className="mt-16 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex gap-8 flex-none">
+            {testimonials.map((testimonial) => (
+              <Card key={testimonial.name} className="max-w-xs">
+                <div className="flex gap-4 items-center">
+                  <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0">
+                    <Image
+                      src={testimonial.avatar}
+                      alt={testimonial.name}
+                      className="max-h-full"
+                    />
+                  </div>
+
+                  <div className="">
+                    <div className="font-semibold">{testimonial.name}</div>
+                    <div className="text-sm text-white/40">
+                      {testimonial.position}
+                    </div>
+                  </div>
+                </div>
+                <p className="mt-4 text-sm">{testimonial.text}</p>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
     </div>
