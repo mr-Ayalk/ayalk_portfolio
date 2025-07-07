242e876c368a3df18f4b2ca2a5a20b3e20d74bd9
mr-Ayalk <ayalkteketel7@gmail.com>
2025-07-07 11:56:09 +0300

    project page completed for tablet size

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -53,20 +53,20 @@ export const ProjectsSection = () => {
             Real-world Results
           </p>
         </div>
-        <h2 className="font-serif text-3xl text-center mt-6">
+        <h2 className="font-serif text-3xl  md:text-5xl text-center mt-6">
           Featured Projects
         </h2>
-        <p className="text-center text-white/60 mt-4">
+        <p className="text-center md:text-lg text-white/60 mt-4 max-w-md mx-auto">
           See how I transformed concepts into engaging digital experiences
         </p>
-        <div className="flex flex-col mt-10 gap-20">
+        <div className="flex flex-col mt-10 md:mt-20 gap-20">
           {portfolioProjects.map((project) => (
             <div
               key={project.title}
               className="bg-gray-800 rounded-3xl relative
               z-0 overflow-hidden after:z-10
               
-              after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline  after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 after:pointer-events-none"
+              after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline  after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8  md:pt-12 md:px-18 after:pointer-events-none"
             >
               <div
                 className="absolute inset-0 -z-10 opacity-5"
@@ -80,21 +80,24 @@ export const ProjectsSection = () => {
                 <span>&bull;</span>
                 <span>{project.year}</span>
               </div>
-              <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
-              <hr className="border-t-2 border-white/5 mt-4" />
-              <ul className="flex flex-col gap-4 mt-4">
+
+              <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">
+                {project.title}
+              </h3>
+              <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+              <ul className="flex flex-col gap-4 mt-4 md:mt-5">
                 {project.results.map((result) => (
                   <li
                     key={result.title}
-                    className="flex gap-2 text-sm text-white/50"
+                    className="flex gap-2 text-sm md:text-base text-white/50"
                   >
-                    <CheckCircleIcon className="size-5" />
+                    <CheckCircleIcon className="size-5 md:size-6" />
                     <span>{result.title}</span>
                   </li>
                 ))}
               </ul>
               <a href={project.link}>
-                <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
                   <span>Visit Live Site</span>
                   <ArrowUpRightIcon className="size-4" />
                 </button>
@@ -102,7 +105,7 @@ export const ProjectsSection = () => {
               <Image
                 src={project.image}
                 alt={project.title}
-                className="mt-8 -mb-4"
+                className="mt-8 -mb-4 md:-mb-0"
               />
             </div>
           ))}
